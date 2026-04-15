ef4ecb37eaaebde8e050c29c1da7b60102d22ca2
naila <nailasalsabila190506@gmail.com>
2026-04-15 11:52:42 +0700

    feat: add database

---
+++ api-hono/src/db.ts
@@ -0,0 +1,10 @@
+import mysql from "mysql2/promise";
+
+export const db = await mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "db_uts_pabp",
+});
+
+console.log("Database Apotikita Connected!");
